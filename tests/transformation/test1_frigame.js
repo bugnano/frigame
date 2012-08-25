654c73ef92eff20f90642e112333b81ce3a423e1
fri <devnull@localhost>
2012-08-25 08:36:52 +0200

    Changed the webgl texture filtering to linear

---
+++ tests/transformation/test1_frigame.js
@@ -0,0 +1,18 @@
+$(function(){
+	var animation = $.friGame.Animation("sh.png", {type: $.friGame.ANIMATION_HORIZONTAL, numberOfFrame: 4, delta: 32, rate: 300});
+
+
+	$.friGame.startGame(function () {
+		$.friGame.playground()
+			.addSprite("rotate",{animation: animation, posx: 0, posy: 16})
+			.addSprite("scale",{animation: animation, posx: 80, posy: 16})
+			.addSprite("rotateScale",{animation: animation, posx: 160, posy: 16})
+			.addSprite("scaleRotate",{animation: animation, posx: 240, posy: 16});
+
+		$.friGame.sprites.rotate.rotate(Math.PI / 4);
+		$.friGame.sprites.scale.scale(2);
+		$.friGame.sprites.rotateScale.rotate(Math.PI / 4).scale(2);
+		$.friGame.sprites.scaleRotate.scale(2).rotate(Math.PI / 4);
+	});
+});
+
