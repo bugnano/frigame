4f216d9f89ae2edfedc24634912b5c0044ff1115
fri <devnull@localhost>
2014-09-16 14:42:37 +0200

    Minor adjustments to the arkanoid demo

--- arkanoid/arkanoid.js
+++ arkanoid/arkanoid.js
@@ -5,14 +5,15 @@
 	'use strict';
 
 	var
-		BRICK_WIDTH = 32,
-		BRICK_HEIGHT = 32,
-		REFRESH_RATE = 30,
-		STATE_STUCK = 0,
-		STATE_MOVING = 1,
-		BALL_SPEED = 8,
-		START_ANGLE = -45 * Math.PI / 180,
-		G = {}
+		G = {
+			BRICK_WIDTH: 32,
+			BRICK_HEIGHT: 32,
+			REFRESH_RATE: 30,
+			STATE_STUCK: 0,
+			STATE_MOVING: 1,
+			BALL_SPEED: 8,
+			START_ANGLE: -45 * Math.PI / 180
+		}
 	;
 
 	$(function () {
@@ -20,10 +21,10 @@
 			.addGradient('top_gradient', {r: 127, g: 191, b: 255})
 			.addGradient('middle_gradient', {r: 127, g: 191, b: 255}, {r: 191, g: 255, b: 255})
 			.addGradient('bottom_gradient', {r: 191, g: 255, b: 255}, {r: 63, g: 255, b: 95})
-			.addAnimation('blue_brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 0 * BRICK_HEIGHT, frameWidth: BRICK_WIDTH, frameHeight: BRICK_HEIGHT})
-			.addAnimation('green_brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 1 * BRICK_HEIGHT, frameWidth: BRICK_WIDTH, frameHeight: BRICK_HEIGHT})
-			.addAnimation('brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 2 * BRICK_HEIGHT, frameWidth: BRICK_WIDTH, frameHeight: BRICK_HEIGHT})
-			.addAnimation('red_brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 3 * BRICK_HEIGHT, frameWidth: BRICK_WIDTH, frameHeight: BRICK_HEIGHT})
+			.addAnimation('blue_brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 0 * G.BRICK_HEIGHT, frameWidth: G.BRICK_WIDTH, frameHeight: G.BRICK_HEIGHT})
+			.addAnimation('green_brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 1 * G.BRICK_HEIGHT, frameWidth: G.BRICK_WIDTH, frameHeight: G.BRICK_HEIGHT})
+			.addAnimation('brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 2 * G.BRICK_HEIGHT, frameWidth: G.BRICK_WIDTH, frameHeight: G.BRICK_HEIGHT})
+			.addAnimation('red_brick', 'bricks.png', {type: fg.ANIMATION_VERTICAL, offsety: 3 * G.BRICK_HEIGHT, frameWidth: G.BRICK_WIDTH, frameHeight: G.BRICK_HEIGHT})
 			.addAnimation('ball', 'ball_large.png')
 			.addAnimation('paddle', 'paddle_middle.png')
 		;
@@ -32,137 +33,124 @@
 			var
 				i,
 				num_bricks,
-				brick_name,
-				playground = fg.playground(),
-				paddle,
-				radius,
-				ball
+				brick_name
 			;
 
-			G.PLAYGROUND_WIDTH = playground.width;
-			G.PLAYGROUND_HEIGHT = playground.height;
-
-			playground
-				.addGroup('top_background', {background: 'top_gradient', left: 0, top: (0 * G.PLAYGROUND_HEIGHT) / 3, width: G.PLAYGROUND_WIDTH, height: G.PLAYGROUND_HEIGHT / 3}).end()
-				.addGroup('middle_background', {background: 'middle_gradient', left: 0, top: (1 * G.PLAYGROUND_HEIGHT) / 3, width: G.PLAYGROUND_WIDTH, height: G.PLAYGROUND_HEIGHT / 3}).end()
-				.addGroup('bottom_background', {background: 'bottom_gradient', left: 0, top: (2 * G.PLAYGROUND_HEIGHT) / 3, width: G.PLAYGROUND_WIDTH, height: G.PLAYGROUND_HEIGHT / 3}).end()
+			fg.playground()
+				.addGroup('top_background', {background: 'top_gradient', left: 0, top: (0 * fg.s.playground.height) / 3, width: fg.s.playground.width, height: fg.s.playground.height / 3}).end()
+				.addGroup('middle_background', {background: 'middle_gradient', left: 0, top: (1 * fg.s.playground.height) / 3, width: fg.s.playground.width, height: fg.s.playground.height / 3}).end()
+				.addGroup('bottom_background', {background: 'bottom_gradient', left: 0, top: (2 * fg.s.playground.height) / 3, width: fg.s.playground.width, height: fg.s.playground.height / 3}).end()
 			;
 
 			G.bricks = {};
-			num_bricks = G.PLAYGROUND_WIDTH / BRICK_WIDTH;
+			num_bricks = fg.s.playground.width / G.BRICK_WIDTH;
 			for (i = 0; i < num_bricks; i += 1) {
 				brick_name = ['red_brick', String(i)].join('');
-				playground.addSprite(brick_name, {animation: 'red_brick', left: i * BRICK_WIDTH, top: 2 * BRICK_HEIGHT});
-				fg.sprites[brick_name].userData = {energy: 2};
-				G.bricks[brick_name] = fg.sprites[brick_name];
+				fg.s.playground.addSprite(brick_name, {animation: 'red_brick', left: i * G.BRICK_WIDTH, top: 2 * G.BRICK_HEIGHT});
+				fg.s[brick_name].userData = {energy: 2};
+				G.bricks[brick_name] = fg.s[brick_name];
 
 				brick_name = ['brick', String(i)].join('');
-				playground.addSprite(brick_name, {animation: 'brick', left: i * BRICK_WIDTH, top: 3 * BRICK_HEIGHT});
-				fg.sprites[brick_name].userData = {energy: 3};
-				G.bricks[brick_name] = fg.sprites[brick_name];
+				fg.s.playground.addSprite(brick_name, {animation: 'brick', left: i * G.BRICK_WIDTH, top: 3 * G.BRICK_HEIGHT});
+				fg.s[brick_name].userData = {energy: 3};
+				G.bricks[brick_name] = fg.s[brick_name];
 
 				brick_name = ['green_brick', String(i)].join('');
-				playground.addSprite(brick_name, {animation: 'green_brick', left: i * BRICK_WIDTH, top: 4 * BRICK_HEIGHT});
-				fg.sprites[brick_name].userData = {energy: 3};
-				G.bricks[brick_name] = fg.sprites[brick_name];
+				fg.s.playground.addSprite(brick_name, {animation: 'green_brick', left: i * G.BRICK_WIDTH, top: 4 * G.BRICK_HEIGHT});
+				fg.s[brick_name].userData = {energy: 3};
+				G.bricks[brick_name] = fg.s[brick_name];
 
 				brick_name = ['blue_brick', String(i)].join('');
-				playground.addSprite(brick_name, {animation: 'blue_brick', left: i * BRICK_WIDTH, top: 5 * BRICK_HEIGHT});
-				fg.sprites[brick_name].userData = {energy: 2};
-				G.bricks[brick_name] = fg.sprites[brick_name];
+				fg.s.playground.addSprite(brick_name, {animation: 'blue_brick', left: i * G.BRICK_WIDTH, top: 5 * G.BRICK_HEIGHT});
+				fg.s[brick_name].userData = {energy: 2};
+				G.bricks[brick_name] = fg.s[brick_name];
 			}
 
-			playground.addSprite('paddle', {animation: 'paddle', centerx: playground.centerx, bottom: playground.bottom - fg.resources.paddle.height});
-			paddle = fg.sprites.paddle;
-			radius = paddle.halfHeight;
-			paddle.userData = {
-				left_circle: fg.Rect({left: paddle.left, centery: paddle.centery, radius: radius}),
-				right_circle: fg.Rect({right: paddle.right, centery: paddle.centery, radius: radius}),
-				middle_square: fg.Rect({centerx: paddle.centerx, centery: paddle.centery, width: paddle.width - (2 * radius), height: paddle.height})
+			fg.s.playground
+				.addSprite('paddle', {animation: 'paddle', centerx: fg.s.playground.centerx, bottom: fg.s.playground.bottom - fg.r.paddle.height})
+				.addSprite('ball', {animation: 'ball', centerx: fg.s.paddle.centerx, bottom: fg.s.paddle.top})
+			;
+
+			fg.s.paddle.userData = {
+				left_circle: fg.Rect({left: fg.s.paddle.left, centery: fg.s.paddle.centery, radius: fg.s.paddle.halfHeight}),
+				right_circle: fg.Rect({right: fg.s.paddle.right, centery: fg.s.paddle.centery, radius: fg.s.paddle.halfHeight}),
+				middle_square: fg.Rect({centerx: fg.s.paddle.centerx, centery: fg.s.paddle.centery, width: fg.s.paddle.width - fg.s.paddle.height, height: fg.s.paddle.height})
 			};
 
-			playground.addSprite('ball', {animation: 'ball', centerx: paddle.centerx, bottom: paddle.top});
-			ball = fg.sprites.ball;
-			ball.userData = {
-				state: STATE_STUCK,
+			fg.s.ball.userData = {
+				state: G.STATE_STUCK,
 				speed_x: 0,
 				speed_y: 0
 			};
 
 			$('#playground').mousedown(function () {
-				if (ball.userData.state === STATE_STUCK) {
-					ball.userData.state = STATE_MOVING;
-					ball.userData.speed_x = BALL_SPEED * Math.cos(START_ANGLE);
-					ball.userData.speed_y = BALL_SPEED * Math.sin(START_ANGLE);
+				if (fg.s.ball.userData.state === G.STATE_STUCK) {
+					fg.s.ball.userData.state = G.STATE_MOVING;
+					fg.s.ball.userData.speed_x = G.BALL_SPEED * Math.cos(G.START_ANGLE);
+					fg.s.ball.userData.speed_y = G.BALL_SPEED * Math.sin(G.START_ANGLE);
 				}
 			});
 
-			playground.registerCallback(function () {
-				var
-					halfWidth = paddle.halfWidth
-				;
-
-				paddle.move({centerx: fg.clamp(fg.mouseTracker.x, halfWidth, G.PLAYGROUND_WIDTH - halfWidth)});
-				paddle.userData.left_circle.move({left: paddle.left});
-				paddle.userData.right_circle.move({right: paddle.right});
-				paddle.userData.middle_square.move({centerx: paddle.centerx});
-				if (ball.userData.state === STATE_STUCK) {
-					ball.move({centerx: paddle.centerx});
+			fg.s.paddle.registerCallback(function () {
+				this.move({centerx: fg.clamp(fg.mouseTracker.x, this.halfWidth, fg.s.playground.width - this.halfWidth)});
+				this.userData.left_circle.move({left: this.left});
+				this.userData.right_circle.move({right: this.right});
+				this.userData.middle_square.move({centerx: this.centerx});
+				if (fg.s.ball.userData.state === G.STATE_STUCK) {
+					fg.s.ball.move({centerx: this.centerx});
 				}
-			}, REFRESH_RATE);
+			}, G.REFRESH_RATE);
 
-			playground.registerCallback(function () {
+			fg.s.ball.registerCallback(function () {
 				var
 					angle,
-					paddle_data = paddle.userData,
-					ball_data = ball.userData,
-					left_circle = paddle_data.left_circle,
-					right_circle = paddle_data.right_circle,
-					middle_square = paddle_data.middle_square
+					left_circle = fg.s.paddle.userData.left_circle,
+					right_circle = fg.s.paddle.userData.right_circle,
+					middle_square = fg.s.paddle.userData.middle_square
 				;
 
-				if (ball.userData.state === STATE_MOVING) {
-					ball.move({centerx: ball.centerx + ball.userData.speed_x, centery: ball.centery + ball.userData.speed_y});
+				if (this.userData.state === G.STATE_MOVING) {
+					this.move({centerx: this.centerx + this.userData.speed_x, centery: this.centery + this.userData.speed_y});
 
 					// Check collision with playground
 					if	(
-							((ball.left <= playground.left) && (ball_data.speed_x < 0))
-						||	((ball.right >= playground.right) && (ball_data.speed_x > 0))
+							((this.left <= fg.s.playground.left) && (this.userData.speed_x < 0))
+						||	((this.right >= fg.s.playground.right) && (this.userData.speed_x > 0))
 						) {
-						ball_data.speed_x *= -1;
+						this.userData.speed_x *= -1;
 					}
 
 					if	(
-							((ball.top <= playground.top) && (ball_data.speed_y < 0))
-						||	((ball.bottom >= playground.bottom) && (ball_data.speed_y > 0))
+							((this.top <= fg.s.playground.top) && (this.userData.speed_y < 0))
+						||	((this.bottom >= fg.s.playground.bottom) && (this.userData.speed_y > 0))
 						) {
-						ball_data.speed_y *= -1;
+						this.userData.speed_y *= -1;
 					}
 
 					// Check collision with paddle
-					if (middle_square.collidePointRect(ball.centerx, ball.bottom) && (ball_data.speed_y > 0)) {
-						ball_data.speed_y *= -1;
-					} else if (middle_square.collidePointRect(ball.centerx, ball.top) && (ball_data.speed_y < 0)) {
-						ball_data.speed_y *= -1;
-					} else if (ball.collideCircle(left_circle)) {
-						angle = Math.atan2(ball.centery - left_circle.centery, ball.centerx - left_circle.centerx);
-						ball_data.speed_x = BALL_SPEED * Math.cos(angle);
-						ball_data.speed_y = BALL_SPEED * Math.sin(angle);
-						if ((ball_data.speed_y >= 0) && (ball_data.speed_y < 1)) {
-							ball_data.speed_y = 1;
+					if (middle_square.collidePointRect(this.centerx, this.bottom) && (this.userData.speed_y > 0)) {
+						this.userData.speed_y *= -1;
+					} else if (middle_square.collidePointRect(this.centerx, this.top) && (this.userData.speed_y < 0)) {
+						this.userData.speed_y *= -1;
+					} else if (this.collideCircle(left_circle)) {
+						angle = Math.atan2(this.centery - left_circle.centery, this.centerx - left_circle.centerx);
+						this.userData.speed_x = G.BALL_SPEED * Math.cos(angle);
+						this.userData.speed_y = G.BALL_SPEED * Math.sin(angle);
+						if ((this.userData.speed_y >= 0) && (this.userData.speed_y < 1)) {
+							this.userData.speed_y = 1;
 						}
-						if ((ball_data.speed_y <= 0) && (ball_data.speed_y > -1)) {
-							ball_data.speed_y = -1;
+						if ((this.userData.speed_y <= 0) && (this.userData.speed_y > -1)) {
+							this.userData.speed_y = -1;
 						}
-					} else if (ball.collideCircle(right_circle)) {
-						angle = Math.atan2(ball.centery - right_circle.centery, ball.centerx - right_circle.centerx);
-						ball_data.speed_x = BALL_SPEED * Math.cos(angle);
-						ball_data.speed_y = BALL_SPEED * Math.sin(angle);
-						if ((ball_data.speed_y >= 0) && (ball_data.speed_y < 1)) {
-							ball_data.speed_y = 1;
+					} else if (this.collideCircle(right_circle)) {
+						angle = Math.atan2(this.centery - right_circle.centery, this.centerx - right_circle.centerx);
+						this.userData.speed_x = G.BALL_SPEED * Math.cos(angle);
+						this.userData.speed_y = G.BALL_SPEED * Math.sin(angle);
+						if ((this.userData.speed_y >= 0) && (this.userData.speed_y < 1)) {
+							this.userData.speed_y = 1;
 						}
-						if ((ball_data.speed_y <= 0) && (ball_data.speed_y > -1)) {
-							ball_data.speed_y = -1;
+						if ((this.userData.speed_y <= 0) && (this.userData.speed_y > -1)) {
+							this.userData.speed_y = -1;
 						}
 					} else {
 						$.noop();
@@ -174,14 +162,14 @@
 							damage = true
 						;
 
-						if (this.collidePointRect(ball.centerx, ball.top) && (ball_data.speed_y < 0)) {
-							ball_data.speed_y *= -1;
-						} else if (this.collidePointRect(ball.centerx, ball.bottom) && (ball_data.speed_y > 0)) {
-							ball_data.speed_y *= -1;
-						} else if (this.collidePointRect(ball.left, ball.centery) && (ball_data.speed_x < 0)) {
-							ball_data.speed_x *= -1;
-						} else if (this.collidePointRect(ball.right, ball.centery) && (ball_data.speed_x > 0)) {
-							ball_data.speed_x *= -1;
+						if (this.collidePointRect(fg.s.ball.centerx, fg.s.ball.top) && (fg.s.ball.userData.speed_y < 0)) {
+							fg.s.ball.userData.speed_y *= -1;
+						} else if (this.collidePointRect(fg.s.ball.centerx, fg.s.ball.bottom) && (fg.s.ball.userData.speed_y > 0)) {
+							fg.s.ball.userData.speed_y *= -1;
+						} else if (this.collidePointRect(fg.s.ball.left, fg.s.ball.centery) && (fg.s.ball.userData.speed_x < 0)) {
+							fg.s.ball.userData.speed_x *= -1;
+						} else if (this.collidePointRect(fg.s.ball.right, fg.s.ball.centery) && (fg.s.ball.userData.speed_x > 0)) {
+							fg.s.ball.userData.speed_x *= -1;
 						} else {
 							damage = false;
 						}
@@ -199,7 +187,7 @@
 						}
 					});
 				}
-			}, REFRESH_RATE);
+			}, G.REFRESH_RATE);
 		});
 	});
 }(jQuery, friGame));
