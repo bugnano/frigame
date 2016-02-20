bd352fef1b994928d96c7a9deaab2f99441bd84d
fri <devnull@localhost>
2016-02-20 11:29:29 +0100

    Simplified the frigame.vec2.js plugin

--- plugins/frigame.vec2.js
+++ plugins/frigame.vec2.js
@@ -26,171 +26,171 @@
 (function (fg) {
 	'use strict';
 
-	var
-		Vec2 = {
-			randomUnit: function (out) {
-				var
-					angle = Math.random() * Math.PI * 2
-				;
+	fg.Vec2 = {
+		randomUnit: function (out) {
+			var
+				angle = Math.random() * Math.PI * 2
+			;
 
-				out.x = Math.cos(angle);
-				out.y = Math.sin(angle);
-			},
+			out.x = Math.cos(angle);
+			out.y = Math.sin(angle);
+		},
 
-			random: function (out, scale) {
-				var
-					mag,
-					angle = Math.random() * Math.PI * 2
-				;
+		random: function (out, scale) {
+			var
+				mag,
+				angle = Math.random() * Math.PI * 2
+			;
 
-				if (scale === undefined) {
-					scale = 1;
-				}
-
-				mag = Math.random() * scale;
-				out.x = Math.cos(angle) * mag;
-				out.y = Math.sin(angle) * mag;
-			},
-
-			clone: function (out, a) {
-				out.x = a.x;
-				out.y = a.y;
-			},
-
-			magnitude: function (a) {
-				var
-					x = a.x,
-					y = a.y
-				;
-
-				return Math.sqrt((x * x) + (y * y));
-			},
-
-			squaredMagnitude: function (a) {
-				var
-					x = a.x,
-					y = a.y
-				;
-
-				return ((x * x) + (y * y));
-			},
-
-			azimuth: function (a) {
-				return Math.atan2(a.y, a.x);
-			},
-
-			fromMagAngle: function (out, mag, angle) {
-				out.x = Math.cos(angle) * mag;
-				out.y = Math.sin(angle) * mag;
-			},
-
-			fromValues: function (out, x, y) {
-				out.x = x;
-				out.y = y;
-			},
-
-			scale: function (out, value) {
-				out.x *= value;
-				out.y *= value;
-			},
-
-			invert: function (out) {
-				out.x *= -1;
-				out.y *= -1;
-			},
-
-			normalize: function (out) {
-				var
-					x = out.x,
-					y = out.y,
-					mag = (x * x) + (y * y)
-				;
-
-				if (mag) {
-					mag = 1 / Math.sqrt(mag);
-				}
-
-				out.x = x * mag;
-				out.y = y * mag;
-			},
-
-			add: function (out, a) {
-				out.x += a.x;
-				out.y += a.y;
-			},
-
-			subtract: function (out, a) {
-				out.x -= a.x;
-				out.y -= a.y;
-			},
-
-			rotate: function (out, angle) {
-				var
-					x = out.x,
-					y = out.y,
-					cos = Math.cos(angle),
-					sin = Math.sin(angle)
-				;
-
-				out.x = (x * cos) - (y * sin);
-				out.y = (y * cos) + (x * sin);
-			},
-
-			rotateAroundPoint: function (out, a, axisPoint, angle) {
-				Vec2.clone(out, a);
-				Vec2.subtract(out, axisPoint);
-				Vec2.rotate(out, angle);
-				Vec2.add(out, axisPoint);
-			},
-
-			equals: function (a, b) {
-				return ((a.x === b.x) && (a.y === b.y));
-			},
-
-			distance: function (a, b) {
-				var
-					dx = a.x - b.x,
-					dy = a.y - b.y
-				;
-
-				return Math.sqrt((dx * dx) + (dy * dy));
-			},
-
-			squaredDistance: function (a, b) {
-				var
-					dx = a.x - b.x,
-					dy = a.y - b.y
-				;
-
-				return ((dx * dx) + (dy * dy));
-			},
-
-			sum: function (out, a, b) {
-				out.x = a.x + b.x;
-				out.y = a.y + b.y;
-			},
-
-			difference: function (out, a, b) {
-				out.x = a.x - b.x;
-				out.y = a.y - b.y;
-			},
-
-			dot: function (a, b) {
-				return ((a.x * b.x) + (a.y * b.y));
-			},
-
-			lerp: function (out, a, b, t) {
-				var
-					ax = a.x,
-					ay = a.y
-				;
-
-				out.x = ax + (t * (b.x - ax));
-				out.y = ay + (t * (b.y - ay));
+			if (scale === undefined) {
+				scale = 1;
 			}
-		}
-	;
 
-	fg.Vec2 = Vec2;
+			mag = Math.random() * scale;
+			out.x = Math.cos(angle) * mag;
+			out.y = Math.sin(angle) * mag;
+		},
+
+		clone: function (out, a) {
+			out.x = a.x;
+			out.y = a.y;
+		},
+
+		magnitude: function (a) {
+			var
+				x = a.x,
+				y = a.y
+			;
+
+			return Math.sqrt((x * x) + (y * y));
+		},
+
+		squaredMagnitude: function (a) {
+			var
+				x = a.x,
+				y = a.y
+			;
+
+			return ((x * x) + (y * y));
+		},
+
+		azimuth: function (a) {
+			return Math.atan2(a.y, a.x);
+		},
+
+		fromMagAngle: function (out, mag, angle) {
+			out.x = Math.cos(angle) * mag;
+			out.y = Math.sin(angle) * mag;
+		},
+
+		fromValues: function (out, x, y) {
+			out.x = x;
+			out.y = y;
+		},
+
+		scale: function (out, value) {
+			out.x *= value;
+			out.y *= value;
+		},
+
+		invert: function (out) {
+			out.x *= -1;
+			out.y *= -1;
+		},
+
+		normalize: function (out) {
+			var
+				x = out.x,
+				y = out.y,
+				mag = (x * x) + (y * y)
+			;
+
+			if (mag) {
+				mag = 1 / Math.sqrt(mag);
+			}
+
+			out.x = x * mag;
+			out.y = y * mag;
+		},
+
+		add: function (out, a) {
+			out.x += a.x;
+			out.y += a.y;
+		},
+
+		subtract: function (out, a) {
+			out.x -= a.x;
+			out.y -= a.y;
+		},
+
+		rotate: function (out, angle) {
+			var
+				x = out.x,
+				y = out.y,
+				cos = Math.cos(angle),
+				sin = Math.sin(angle)
+			;
+
+			out.x = (x * cos) - (y * sin);
+			out.y = (y * cos) + (x * sin);
+		},
+
+		rotateAroundPoint: function (out, a, axisPoint, angle) {
+			var
+				Vec2 = fg.Vec2
+			;
+
+			Vec2.clone(out, a);
+			Vec2.subtract(out, axisPoint);
+			Vec2.rotate(out, angle);
+			Vec2.add(out, axisPoint);
+		},
+
+		equals: function (a, b) {
+			return ((a.x === b.x) && (a.y === b.y));
+		},
+
+		distance: function (a, b) {
+			var
+				dx = a.x - b.x,
+				dy = a.y - b.y
+			;
+
+			return Math.sqrt((dx * dx) + (dy * dy));
+		},
+
+		squaredDistance: function (a, b) {
+			var
+				dx = a.x - b.x,
+				dy = a.y - b.y
+			;
+
+			return ((dx * dx) + (dy * dy));
+		},
+
+		sum: function (out, a, b) {
+			out.x = a.x + b.x;
+			out.y = a.y + b.y;
+		},
+
+		difference: function (out, a, b) {
+			out.x = a.x - b.x;
+			out.y = a.y - b.y;
+		},
+
+		dot: function (a, b) {
+			return ((a.x * b.x) + (a.y * b.y));
+		},
+
+		lerp: function (out, a, b, t) {
+			var
+				ax = a.x,
+				ay = a.y
+			;
+
+			out.x = ax + (t * (b.x - ax));
+			out.y = ay + (t * (b.y - ay));
+		}
+	};
 }(friGame));
 
