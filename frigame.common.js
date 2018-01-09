25d3bff904a016b0ae21f6c20345265a76bc33ea
fri <devnull@localhost>
2018-01-09 09:43:00 +0100

    In frigame.common.js, set the global friGame object even when using AMD

--- frigame.common.js
+++ frigame.common.js
@@ -29,7 +29,9 @@
 
 	if ((typeof define === 'function') && define.amd) {
 		// AMD. Register as an anonymous module.
-		define([], factory);
+		define([], function () {
+			return (root.friGame = factory());
+		});
 	} else if ((typeof module === 'object') && module.exports) {
 		// Node. Does not work with strict CommonJS, but
 		// only CommonJS-like environments that support module.exports,
